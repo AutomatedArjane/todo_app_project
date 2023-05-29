f1b8579fcc3905a5224bf6d9447bac2af127fc83
Arjane JAMK <ad1945@student.jamk.fi>
2023-05-29 14:53:28 +0300

    Installing npm packages

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-console.log("Hello World")
+const express = require('express') 
+const cors = require('cors')
+const app = express()
+const port = 3000
+
+// cors - allow connection from different domains and ports
+app.use(cors())
+
+// convert json string to json object (from request)
+app.use(express.json())
+
+// mongo here...
+
+// todos-route
+app.get('/todos', (request, response) => {
+  response.send('Todos')
+})
+
+// app listen port 3000
+app.listen(port, () => {
+  console.log('Example app listening on port 3000')
+})
