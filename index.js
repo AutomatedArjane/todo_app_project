d43528a7d2ced27c4de33ef25dc6bf01d9942ac1
Arjane JAMK <ad1945@student.jamk.fi>
2023-05-29 16:31:52 +0300

    GET and POST commands work

--- index.js
+++ index.js
@@ -10,11 +10,40 @@ app.use(cors())
 app.use(express.json())
 
 // mongo here...
+const mongoose = require('mongoose')
+const mongoDB = 'mongodb+srv://Arjane:XGkbfnnhZLJN1nnq@cluster0.s7be9tp.mongodb.net/todo_app_database?retryWrites=true&w=majority'
+mongoose.connect(mongoDB, {useNewUrlParser: true, useUnifiedTopology: true})
+
+const db = mongoose.connection
+db.on('error', console.error.bind(console, 'connection error:'))
+db.once('open', function() {
+  console.log("Database test connected")
+})
+
+// Mongoose Schema and Model here...
+// schema
+const todoSchema = new mongoose.Schema({
+    text: { type: String, required: true } 
+  })
+  
+  // model
+  const Todo = mongoose.model('Todo', todoSchema, 'todos')
+  
+  // Routes here...
 
 // todos-route
 app.get('/todos', (request, response) => {
   response.send('Todos')
 })
+
+app.post('/todos', async (request, response) => {
+    const { text } = request.body
+    const todo = new Todo({
+      text: text
+    })
+    const savedTodo = await todo.save()
+    response.json(savedTodo)  
+  })
 
 // app listen port 3000
 app.listen(port, () => {
