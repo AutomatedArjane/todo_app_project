fe8114f9826f319a8a95a3e51562b664e68e22c3
Arjane JAMK <ad1945@student.jamk.fi>
2023-05-30 16:19:00 +0300

    Added functions to create a working edit button

--- front_side/code.js
+++ front_side/code.js
@@ -14,84 +14,140 @@ async function loadTodos() {
 function createTodoListItem(todo) {
     // create a new li-element
     let li = document.createElement('li')
-      // create a new id-attribute
+    // create a new id-attribute
     let li_attr = document.createAttribute('id')
-      // attach the value of the task/todo ID to the created attribute
+    // attach the value of the task/todo ID to the created attribute
     li_attr.value = todo._id
-      // attach the attribute to the li-element
+    // attach the attribute to the li-element
     li.setAttributeNode(li_attr)
-      // create a new text node containing the task/todo text
+    // create a new text node containing the task/todo text
     let text = document.createTextNode(todo.text)
-      // add the text to the li-element
+    // add the text to the li-element
     li.appendChild(text)
-      // create a new span-element, with the letter X inside it, to delete tasks
-    let span = document.createElement('span')
-      // create a new class-attribute
-    let span_attr = document.createAttribute('class')
-      /* attach the 'delete' value to the attribute, i.e. the class is 
-      'delete', so the 'delete' class styles can be applied */
-    span_attr.value = 'delete'
-      // add the attribute to the span-element
-    span.setAttributeNode(span_attr)
-      // create a text node containing the letter X
+    // create a new span-element, with the letter X inside it, to delete tasks
+    // EDIT function
+    let span_edit = document.createElement('span')
+    // create a new class-attribute
+    let span_edit_attr = document.createAttribute('class')
+    /* attach the 'delete' value to the attribute, i.e. the class is 
+    'delete', so the 'delete' class styles can be applied */
+    span_edit_attr.value = 'edit'
+    // add the attribute to the span-element
+    span_edit.setAttributeNode(span_edit_attr)
+    // create a text node containing the letter X
+    let edit = document.createTextNode(' edit ')
+    // attach the x-span node to the span element (visible)
+    span_edit.appendChild(edit)
+    // configure the span-element's onclick-event to call the editTodo function
+    span_edit.onclick = function () { editTodo(todo._id) }
+    // add a span-element to the li-element
+    li.appendChild(span_edit)
+    // return the created li-element
+    // DELETE function
+    let span_del = document.createElement('span')
+    // create a new class-attribute
+    let span_del_attr = document.createAttribute('class')
+    /* attach the 'delete' value to the attribute, i.e. the class is 
+    'delete', so the 'delete' class styles can be applied */
+    span_del_attr.value = 'delete'
+    // add the attribute to the span-element
+    span_del.setAttributeNode(span_del_attr)
+    // create a text node containing the letter X
     let x = document.createTextNode(' x ')
-      // attach the x-span node to the span element (visible)
-    span.appendChild(x)
-      // configure the span-element's onclick-event to call the removeTodo function
-    span.onclick = function() { removeTodo(todo._id) }
-      // add a span-element to the li-element
-    li.appendChild(span)
-      // return the created li-element
-      // it then has the form: <li id="mongoIDXXXXX">Remember to call...<span class="remove">x</span></li>
+    // attach the x-span node to the span element (visible)
+    span_del.appendChild(x)
+    // configure the span-element's onclick-event to call the removeTodo function
+    span_del.onclick = function () { removeTodo(todo._id) }
+    // add a span-element to the li-element
+    li.appendChild(span_del)
+    // return the created li-element
+    // it then has the form: <li id="mongoIDXXXXX">Remember to call...<span class="remove">x</span></li>
     return li
-  }
+}
 
-  function showTodos(todos) {
+function showTodos(todos) {
     let todosList = document.getElementById('todosList')
     let infoText = document.getElementById('infoText')
     // no todos
     if (todos.length === 0) {
-      infoText.innerHTML = 'No todos'
-    } else {    
-      todos.forEach(todo => {
-          let li = createTodoListItem(todo)        
-          todosList.appendChild(li)
-      })
-      infoText.innerHTML = ''
+        infoText.innerHTML = 'No todos'
+    } else {
+        todos.forEach(todo => {
+            let li = createTodoListItem(todo)
+            todosList.appendChild(li)
+        })
+        infoText.innerHTML = ''
     }
-  }
+}
 
-  async function addTodo() {
+async function addTodo() {
     let newTodo = document.getElementById('newTodo')
     const data = { 'text': newTodo.value }
     const response = await fetch('http://localhost:3000/todos', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(data)
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
     })
     let todo = await response.json()
     let todosList = document.getElementById('todosList')
     let li = createTodoListItem(todo)
     todosList.appendChild(li)
-  
+
     let infoText = document.getElementById('infoText')
     infoText.innerHTML = ''
     newTodo.value = ''
-  }
+}
 
-  async function removeTodo(id) {
-    const response = await fetch('http://localhost:3000/todos/'+id, {
-      method: 'DELETE'
+async function removeTodo(id) {
+    const response = await fetch('http://localhost:3000/todos/' + id, {
+        method: 'DELETE'
     })
     let responseJson = await response.json()
     let li = document.getElementById(id)
     li.parentNode.removeChild(li)
-  
+
     let todosList = document.getElementById('todosList')
     if (!todosList.hasChildNodes()) {
-      let infoText = document.getElementById('infoText')
-      infoText.innerHTML = 'No todos'
+        let infoText = document.getElementById('infoText')
+        infoText.innerHTML = 'No todos'
     }
-  }
+}
+
+async function editTodo(id) {
+    var addButton = document.querySelector('button[onclick="addTodo()"]');
+    addButton.setAttribute("onclick", `saveEditedTodo('${id}')`);
+    addButton.textContent = "Save";
+    addButton.id = "save-button";
+    
+    let editedTodo = document.getElementById('newTodo');
+
+    let li = document.getElementById(id);
+    let todoText = li.firstChild.nodeValue;
+
+    editedTodo.value = todoText;
+}
+
+async function saveEditedTodo(id) {
+    let editedTodo = document.getElementById('newTodo');
+    const data = { 'text': editedTodo.value };
+    const response = await fetch('http://localhost:3000/todos/' + id, {
+        method: 'PUT',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    })
+    
+    if (response.ok) {
+        let li = document.getElementById(id);
+        li.firstChild.nodeValue = editedTodo.value;
+        editedTodo.value = '';
+
+        var addButton = document.querySelector('button[onclick="saveEditedTodo(\'' + id + '\')"]');
+        addButton.setAttribute("onclick", "addTodo()");
+        addButton.textContent = "Add";
+        addButton.id = "edit-button";
+    }
+}
